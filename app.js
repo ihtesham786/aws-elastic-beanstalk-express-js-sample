2c346f81a19eaf6c1c6a019f47ec2ef6eded86e1
aws-test <4332152+ihtesham786@users.noreply.github.com>
2020-10-10 08:57:08 +0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is a test file for aws'));
+app.get('/', (req, res) => res.send('Full pipeline test - Manual Approval Required'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
